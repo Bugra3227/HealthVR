38dabe25a52ec84bc60087a91e44e043545c0771
Bugrahan Yilmaz <77055795+Bugra3227@users.noreply.github.com>
2025-11-15 13:10:12 +0300

    Update CorrectArea.cs

--- Assets/Scripts/CorrectArea.cs
+++ Assets/Scripts/CorrectArea.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Detects when the player enters the correct area on the path.
+/// Once triggered, it notifies PathTracking and increases the correct-path counter.
+/// </summary>
 public class CorrectArea : MonoBehaviour
 {
     private bool _pasiveArea;
